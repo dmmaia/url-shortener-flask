3b04c60be66b4b35a355c278d47cf2f03c646496
dmmaia <david_mmaia@hotmail.com>
2023-08-25 10:30:38 -0300

    feat: get url input value

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask, render_template
+import random
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        urlBase = request.form.get("urlInput")
+
+        try:
+            response = requests.get(urlBase)
+        except:
+            return render_template("index.html", urlShortenned = "")    
+        
+        if response.status_code == 200:
+            return render_template("index.html", urlShortenned=generateUrl(urlBase, request.url))
+        else:
+            return render_template("index.html", urlShortenned = "")    
+    return render_template("index.html", urlShortenned="")
+
+def generateUrl(urlBase, url):
+    return url+str(random.randrange(1000,9999))
